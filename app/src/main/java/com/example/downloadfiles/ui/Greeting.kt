a5eb338f6278d53335dd4707bdc472a9dd1ac2af
Khaled.Eid <Khaled.Eid@souhoola.com>
2024-08-08 15:09:25 +0200

    lol 7 done

--- app/src/main/java/com/example/downloadfiles/ui/Greeting.kt
+++ app/src/main/java/com/example/downloadfiles/ui/Greeting.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -49,10 +50,6 @@ fun Greeting(modifier: Modifier = Modifier) {
     var mService by remember { mutableStateOf<DownloadService?>(null) }
     var mBound by remember { mutableStateOf(false) }
     val context = LocalContext.current
-    val updateNotificationProcess: MutableMap<Int, MutableState<Int> > =
-        remember { mutableMapOf() }
-
-
     DisposableEffect(key1 = Unit) {
         val connection = object : ServiceConnection {
             override fun onServiceConnected(className: ComponentName, service: IBinder) {
@@ -74,140 +71,38 @@ fun Greeting(modifier: Modifier = Modifier) {
             mBound = false
         }
     }
-//
-//    LaunchedEffect(key1 = mService?.stateFlows) {
-//        if (mBound) {
-//            mService?.updateNotificationProcess?.collectLatest {
-//                mService?.let { mService ->
-//                    Log.d("adsad3663", "mService $mService")
-//                    Log.d("adsad3663", "stateFlows ${mService.stateFlows[1]}")
-//                    mService.stateFlows[1]?.collectLatest {
-//                        Log.d("adsad3663", "Collected value: $it")
-//                    }
-//                } ?: Log.d("adsad3663", "mService = null")
-//            }
-//        }
-//    }
-
-
-    LaunchedEffect(key1 = mService?.stateFlows) {
-        if (mBound) {
-            mService?.updateNotificationProcess?.collectLatest {
-                mService?.stateFlows?.get(1)?.collectLatest {lol->
-                    updateNotificationProcess[1]?.let {
-                        it.value = lol
-                    } ?:Log.d("adsadddddddda44sdsad", "1 SharedDataHolder:  updateNotificationProcess[1] = null   ")
-                    Log.d("adsadddddddda44sdsad", "1 SharedDataHolder:${it}  ")
-                    Log.d("adsadddddddda44sdsad", "1 SharedDataHolder:${ updateNotificationProcess[1]?.value}  ")
-
-                }
-            }
-        }
-
-    }
-//    LaunchedEffect(key1 = mService?.stateFlows) {
-//        Log.d("adsadddddddda44sdsad", "2 SharedDataHolder:$ aaaa ")
-//
-//        if (mBound) {
-//            Log.d("adsadddddddda44sdsad", "2 SharedDataHolder:$ mBound ")
-//             mService?.stateFlows?.get(2)?.collectLatest {
-//                Log.d("adsadddddddda44sdsad", "2 SharedDataHolder:${it}  ")
-//
-//            }
-//        }
-//
-//
-//    }
-//    LaunchedEffect(key1 =mService?.stateFlows) {
-//        Log.d("adsadddddddda44sdsad", "3 SharedDataHolder:$ aaaa ")
-//
-//        if (mBound) {
-//            Log.d("adsadddddddda44sdsad", "3 SharedDataHolder:$ mBound ")
-//
-//            mService?.stateFlows?.get(3)?.collectLatest {
-//                Log.d("adsadddddddda44sdsad", "3 SharedDataHolder:${it}  ")
-//
-//            }
-//        }
-//    }
-
-
-
-
-
     Column(
         modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly,
         Alignment.CenterHorizontally
     ) {
-        updateNotificationProcess[1] = remember {
-            mutableIntStateOf (0)
-        }
         DownloadFile(
-            updateNotificationProcess[1]?.value?:0,
             context,
             "https://server8.mp3quran.net/harthi/014.mp3",
             1,
-            { url, nationalId ->
-                mService?.startDownload("ssssadsssss", url, nationalId)
-            }) { key ->
-//            LaunchedEffect(key1 = mService?.stateFlows?.get(key)) {
-//                Log.d("TAG", "saadsadsad:${updateNotificationProcess[1]?.value} , $key")
-//                if (mBound) {
-//                    mService?.stateFlows?.get(1)?.collectLatest {
-//                        updateNotificationProcess[1]?.value = it
-//                    }
-//                } else {
-//
-//
-//                }
-//
-//            }
+            mService=mService,
+            mBound=mBound
+        ) { url, nationalId ->
+            mService?.startDownload("45AD1q", url, nationalId)
         }
-//        DownloadFile(updateNotificationProcess[2]?.value ?: 0,
-//            context,
-//            "https://server8.mp3quran.net/harthi/015.mp3",
-//            2,
-//            { url, nationalId ->
-//                mService?.startDownload("2d1a", url, nationalId)
-//            }
-//        ) { key ->
-//            LaunchedEffect(key1 = mService?.stateFlows?.get(2)) {
-//                Log.d("TAG", "saadsadsad:${updateNotificationProcess[key]?.value} , $key")
-//
-//                if (mBound) {
-//                    mService?.stateFlows?.get(2)?.collectLatest {
-//                        updateNotificationProcess[2]?.value = it
-//                    }
-//                } else {
-//
-//
-//                }
-//
-//            }
-//        }
-//        DownloadFile(updateNotificationProcess[3]?.value ?: 0,
-//            context,
-//            "https://server8.mp3quran.net/harthi/016.mp3",
-//            3,
-//            { url, nationalId ->
-//                mService?.startDownload("3d1a", url, nationalId)
-//            }
-//        ) { key ->
-//
-//            LaunchedEffect(key1 = mService?.stateFlows?.get(key)) {
-//                Log.d("TAG", "saadsadsad:${updateNotificationProcess[key]?.value} , $key")
-//
-//                if (mBound) {
-//                    mService?.stateFlows?.get(key)?.collectLatest {
-//                        updateNotificationProcess[key]?.value = it
-//                    }
-//                } else {
-//
-//
-//                }
-//
-//            }
-//        }
+        DownloadFile(
+            context,
+            "https://server8.mp3quran.net/harthi/014.mp3",
+            2,
+            mService=mService,
+            mBound=mBound
+        ) { url, nationalId ->
+            mService?.startDownload("45AD2q", url, nationalId)
+        }
+        DownloadFile(
+            context,
+            "https://server8.mp3quran.net/harthi/014.mp3",
+            3,
+            mService=mService,
+            mBound=mBound
+        ) { url, nationalId ->
+            mService?.startDownload("45AD3q", url, nationalId)
+        }
+
 
 
     }
@@ -216,16 +111,25 @@ fun Greeting(modifier: Modifier = Modifier) {
 
 @Composable
 fun DownloadFile(
-    progress: Int,
     context: Context,
     url: String,
     nationalId: Int,
+    mService: DownloadService?,
+    mBound: Boolean,
     startDownload: (String, Int) -> Unit,
-    updateState: @Composable (Int) -> Unit
-) {
-    Log.d("TAG", "asdsa4d58sa4d: $progress")
 
-    updateState(nationalId)
+) {
+    val progress = remember { mutableIntStateOf (0) }
+    LaunchedEffect(key1 = mService?.stateFlows) {
+        if (mBound) {
+            mService?.updateNotificationProcess?.collectLatest {
+                mService.stateFlows[nationalId]?.collectLatest {
+                    progress.intValue=it
+                }
+            }
+        }
+
+    }
     Button(
         onClick = {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -240,7 +144,7 @@ fun DownloadFile(
         Text(text = "Download")
     }
     Spacer(modifier = Modifier.height(16.dp))
-    if (progress == 100) {
+    if (progress.intValue == 100) {
         Image(
             modifier = Modifier.size(100.dp),
             painter = painterResource(id = R.drawable.done),
@@ -248,15 +152,13 @@ fun DownloadFile(
         )
     } else {
         Box {
-            Log.d("TAG", "asdsa4d58sa4d: $progress")
-
             CircularProgressIndicator(
                 trackColor = androidx.compose.ui.graphics.Color.Red,
-                progress = progress.toFloat(),
+                progress = progress.intValue .toFloat(),
                 modifier = Modifier.size(100.dp)
             )
             Text(
-                text = progress.toString(),
+                text = progress.intValue.toString(),
                 modifier = Modifier.align(Alignment.Center)
             )
         }
